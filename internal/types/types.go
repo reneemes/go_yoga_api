0dbc6888040b8efba58667f4fd42942ba8c5592b
Renee Messersmith <morganrmessersmith@gmail.com>
2025-08-07 15:37:37 -0400

    Feat: created a types file to store structs in to keep code DRY

---
+++ internal/types/types.go
@@ -0,0 +1,19 @@
+package types
+
+type Pose struct {
+	ID   						int    `json:"id"`
+	Name 						string `json:"name"`
+	SanskritName 		string `json:"sanskrit_name"`
+	TranslationName string `json:"translation_name"`
+	Description 		string `json:"description"`
+	PoseBenefits 		string `json:"pose_benefits"`
+	ImageURL 				string `json:"image_url"`
+}
+
+type Routine struct {
+	ID          	int    	`json:"id"`
+	Name					string	`json:"name"`
+	Description 	string 	`json:"description"`
+	Difficulty 		string 	`json:"difficulty"`
+	RoutinePoses 	[]Pose 	`json:"routine_poses"`
+}
