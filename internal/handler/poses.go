cd039c552c212d257cfb11990b20c029577baae3
Renee Messersmith <morganrmessersmith@gmail.com>
2025-08-07 15:38:48 -0400

    Feat: will return a pose based on its ID number

--- internal/handler/poses.go
+++ internal/handler/poses.go
@@ -1,21 +1,15 @@
 package handler
 
 import (
-	"net/http"
-	"github.com/gin-gonic/gin"
+	// "fmt"
 	"go_yoga_api/internal/database"
+	"go_yoga_api/internal/types"
+	// "log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	// ^ Import the database package to access the service
 )
-
-type Pose struct {
-	ID   						int    `json:"id"`
-	Name 						string `json:"name"`
-	SanskritName 		string `json:"sanskrit_name"`
-	TranslationName string `json:"translation_name"`
-	Description 		string `json:"description"`
-	PoseBenefits 		string `json:"pose_benefits"`
-	ImageURL 				string `json:"image_url"`
-}
 
 func GetAllPosesHandler(c *gin.Context) {
 	db := database.New() // Create a new database service instance
@@ -30,9 +24,9 @@ func GetAllPosesHandler(c *gin.Context) {
 	// This prevents resource leaks
 	defer rows.Close()
 
-	var poses []Pose
+	var poses []types.Pose
 	for rows.Next() {
-		var p Pose
+		var p types.Pose
 		if err := rows.Scan(&p.ID, &p.Name, &p.SanskritName, &p.TranslationName, &p.Description, &p.PoseBenefits, &p.ImageURL); err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": "Failed to scan pose",
@@ -44,5 +38,36 @@ func GetAllPosesHandler(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"data": poses,
+	})
+}
+
+func GetOnePoseHandler(c *gin.Context) {
+	query := `
+		SELECT id, name, sanskrit_name, translation_name, description, pose_benefits, image_url
+		FROM poses
+		WHERE id = $1
+	`
+
+	id := c.Param("id")
+	// fmt.Println("Fetching pose with ID:", id)
+	db := database.New()
+	row := db.DB().QueryRow(query, id)
+
+	var p types.Pose
+	if err := row.Scan(&p.ID, &p.Name, &p.SanskritName, &p.TranslationName, &p.Description, &p.PoseBenefits, &p.ImageURL); err != nil {
+		// fmt.Println("Error fetching pose with ID:", id, err)
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Pose not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": p,
+		// "data": {
+		// 	"id": p.ID,
+		// 	"type": "pose",
+		// 	"attributes": p, 
+		// }
 	})
 }
